031041b4a8179ac799c5fd357af017e97280747e
none <none@ee.ee>
2020-09-18 10:22:42 +0300

    Среднее арифмит (не доделаное)

--- src/sptvr19weather/App.java
+++ src/sptvr19weather/App.java
@@ -6,6 +6,7 @@
 package sptvr19weather;
 
 import java.util.Random;
+
 import java.util.Scanner;
 
 /**
@@ -18,9 +19,38 @@ class App {
         int min = -30;
         int max = 30;
         int n = 0;
+        int array[][] = new int[12][];
+        int rand;
+        
+        array[0] =  new int[31];
+        array[1] =  new int[28];
+        array[2] =  new int[31];
+        array[3] =  new int[30];
+        array[4] =  new int[31];
+        array[5] =  new int[30];
+        array[6] =  new int[31];
+        array[7] =  new int[31];
+        array[8] =  new int[30];
+        array[9] =  new int[31];
+        array[10] =  new int[30];
+        array[11] =  new int[31];
+        
+        int sum1 = 73;
+        int sum2 = 0;
+        int sum3 = 0;
+        int sum4 = 0;
+        int sum5 = 0;
+        int sum6 = 0;
+        int sum7 = 0;
+        int sum8 = 0;
+        int sum9 = 0;
+        int sum10 = 0;
+        int sum11 = 0;
+        int sum12 = 0;
+        
         Random random = new Random();
         int[][] tempInYear = new int[12][];
-        //строка
+
         System.out.print("     ");
         for (int i = 1; i < 32; i++) {
             System.out.printf("%4d",i);
@@ -44,9 +74,10 @@ class App {
                 case 8: min=5; max=20; n=30; break;
                 case 9: min=-5; max=10; n=31; break;
                 case 10: min=-10; max=0; n=30; break;
-                case 11: min=-15; max=-5; n=31; break;
-                
+                case 11: min=-15; max=-5; n=31; break; 
             }
+            
+            // ячейки для месяца
             tempInYear[i] = new int[n];
             System.out.printf("%4d) ",i+1);
             for (int j = 0; j < tempInYear[i].length; j++) {
@@ -60,6 +91,16 @@ class App {
         int month = scanner.nextInt();
         System.out.print("День месяца: ");
         int day = scanner.nextInt();
-        System.out.printf("В этот день температура была %4d градусов%n", tempInYear[month-1][day-1]);
+        System.out.printf("В этот день температура была: %4d градусов%n", tempInYear[month-1][day-1]);
+       
+        System.out.println("Средняя температура в 1 месяце была: " + sum1 / 31);
+        System.out.println("Средняя температура во 2 месяце была: " + sum2 / 28);
+        System.out.println("Средняя температура в 3 месяце была: " + sum3 / 31);
+        System.out.println("Средняя температура в 4 месяце была: "  + sum4 / 30);
+        System.out.println("Средняя температура в 5 месяце была: " + sum5 / 31);
+        System.out.println("Средняя температура в 6 месяце была: " + sum6 / 30);
+        System.out.println("Средняя температура в 7 месяце была:" + sum7 / 31 );
+        
     }
 }
+
